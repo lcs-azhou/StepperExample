26bade42fc256605c574b98d5b56879271a00cc4
Ansheng Zhou <azhou@lcs.on.ca>
2023-11-16 09:39:36 -0500

    finished

--- StepperExample/ContentView.swift
+++ StepperExample/ContentView.swift
@@ -22,17 +22,17 @@ struct ContentView: View {
             
             HStack(alignment: .top) {
 
-                Text("\(base)")
-                    .font(.system(size: 96))
+                Text("(\(base))")
+                    .font(.system(size: 80))
 
                 Text("2")
                     .font(.system(size: 44))
 
                 Text("=")
-                    .font(.system(size: 96))
+                    .font(.system(size: 80))
 
                 Text("\(squared)")
-                    .font(.system(size: 96))
+                    .font(.system(size: 80))
             }
             Stepper(value: $base, label: {
                 Text("Base")
@@ -45,5 +45,35 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    TabView(selection: Binding.constant(3)) {
+        
+        Text("Plus")
+            .tabItem {
+                Image(systemName: "plus")
+                Text("Plus")
+            }
+            .tag(1)
+        
+        Text("Minus")
+            .tabItem {
+                Image(systemName: "minus")
+                Text("Minus")
+            }
+            .tag(2)
+        
+        ContentView()
+            .tabItem {
+                Image(systemName: "multiply")
+                Text("Multiply")
+            }
+            .tag(3)
+        
+        Text("divide")
+            .tabItem {
+                Image(systemName: "divide")
+                Text("Divide")
+            }
+            .tag(4)
+        
+    }.accentColor(.purple)
 }
